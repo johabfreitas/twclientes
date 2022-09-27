7056c82eceb4332fbb097f8e8f4cedad4af5dfb4
Johab Freitas <freitasjohab@gmail.com>
2022-09-27 13:02:00 -0300

    First commit

---
+++ src/main/java/br/com/johabfreitas/twclientes/TwclientesApplication.java
@@ -0,0 +1,13 @@
+package br.com.johabfreitas.twclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwclientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwclientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/johabfreitas/twclientes/TwclientesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.johabfreitas.twclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwclientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
